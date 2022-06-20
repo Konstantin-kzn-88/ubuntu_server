187a48e18cf0ae02d6adf977e3a8433ff9cd917c
Konstantin <kuznetsovkm@yandex.ru>
2022-06-20 17:45:04 +0300

    Почистил от файлов

--- local_client2.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 8888))
-sock.send(b'(1, [1, 3, 4])')
-res = sock.recv(64)
-print(res.decode())
-sock.close()
